68551ae569e01a25d3ffbf4de059df739beef6cf
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-08-07 18:44:59 +0200

    add descr and create data

---
+++ data/projects.js
@@ -0,0 +1,42 @@
+export const projectsArray = [
+  {
+    id: 1,
+    name: "Phonebook",
+    description:
+      "Phonebook - the Searchable Phone Book project is a web application that allows users to store and search for phone book entries. The application is built using a combination of technologies, including HTML, JavaScript, React, Redux, REST API, and Material UI.",
+    role: "Developer",
+    image: "/Phonebook.png",
+    github: "https://github.com/eugeniusz57/goit-react-hw-08-phonebook",
+    link: "https://eugeniusz57.github.io/goit-react-hw-08-phonebook/contacts",
+  },
+  {
+    id: 2,
+    name: "So Yummy",
+    description:
+      'SoYummy - the "Website with Regulations from All Over the World" project is a web application that allows users to access various legal regulations and laws from different countries around the globe. The application leverages a wide range of technologies, including HTML, JavaScript, React, Redux, REST API, Node.js, own database, Express, MongoDB, and Styled Components.',
+    role: "Developer",
+    image: "/SoYummy.png",
+    github: "https://github.com/AlexTanetsDev/yummy-team4-react-project",
+    link: "https://team-team-yummy.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Filmoteka ",
+    description:
+      'The "Filmoteka" offers users a convenient way to browse and select movies for watching. Utilizing technologies like HTML, SASS, JavaScript, REST API, and Parcel, the project delivers an attractive and user-friendly interface, making it easy for users to find interesting movies and ensuring an enjoyable experience while using the application.',
+    role: "Developer",
+    image: "/Filmoteka.png",
+    github: "https://github.com/Reqvite/Filmoteka",
+    link: "https://reqvite.github.io/Filmoteka/",
+  },
+  {
+    id: 4,
+    name: "Web studio",
+    description:
+      "Web Studio is a website that offers a responsive layout, designed to adapt to different devices and screen sizes. The project utilizes HTML, SCSS (Sassy CSS), and JavaScript technologies.",
+    role: "Developer",
+    image: "/Webstudio.png",
+    github: "https://github.com/eugeniusz57/goit-markup-hw-08",
+    link: "https://eugeniusz57.github.io/goit-markup-hw-08/",
+  },
+];
